73cb9545dd4b2fd3e58822bab069907fea70c5bd
Arataxa <131537929+Arataxa@users.noreply.github.com>
2024-07-12 20:28:09 +0500

    Update shoot.py

--- sprint3/problems/flamegraph/solution/shoot.py
+++ sprint3/problems/flamegraph/solution/shoot.py
@@ -3,7 +3,6 @@ import subprocess
 import time
 import random
 import shlex
-import os
 
 RANDOM_LIMIT = 1000
 SEED = 123456789
@@ -16,32 +15,41 @@ AMMUNITION = [
 
 SHOOT_COUNT = 100
 COOLDOWN = 0.1
-FLAMEGRAPH_REPO = 'https://github.com/brendangregg/FlameGraph.git'
-FLAMEGRAPH_DIR = 'FlameGraph'
 
+PERF_DATA = "perf.data"
+RESULT_SVG = "graph.svg"
 
 def start_server():
     parser = argparse.ArgumentParser()
-    parser.add_argument('server', type=str)
+    parser.add_argument('server', type=str, help='Command to start the server')
     return parser.parse_args().server
 
-
-def run(command, output=None):
-    process = subprocess.Popen(shlex.split(command), stdout=output, stderr=subprocess.DEVNULL)
+def run(command, output=None, shell=False):
+    process = subprocess.Popen(shlex.split(command) if not shell else command, stdout=output, stderr=subprocess.DEVNULL, shell=shell)
     return process
-
 
 def stop(process, wait=False):
     if process.poll() is None and wait:
         process.wait()
     process.terminate()
 
+def record_perf(pid):
+    perf_command = f'sudo perf record -o ./{PERF_DATA} -p {pid} -g'
+    perf_process = run(perf_command)
+    return perf_process
+
+def convert_to_flamegraph():
+    collapse = f'./FlameGraph/stackcollapse-perf.pl'
+    flamegraph = f'./FlameGraph/flamegraph.pl'
+ 
+    convert_command = f'sudo perf script -i ./{PERF_DATA} | {collapse} | {flamegraph} > ./{RESULT_SVG}'
+    convert_process = run(convert_command, shell=True)
+    return convert_process
 
 def shoot(ammo):
     hit = run('curl ' + ammo, output=subprocess.DEVNULL)
     time.sleep(COOLDOWN)
     stop(hit, wait=True)
-
 
 def make_shots():
     for _ in range(SHOOT_COUNT):
@@ -49,46 +57,16 @@ def make_shots():
         shoot(AMMUNITION[ammo_number])
     print('Shooting complete')
 
+server = run(start_server())  
+perf_process = record_perf(server.pid)
 
-def clone_flamegraph_repo():
-    if not os.path.exists(FLAMEGRAPH_DIR):
-        subprocess.run(['git', 'clone', FLAMEGRAPH_REPO])
-    else:
-        print(f"{FLAMEGRAPH_DIR} already exists.")
+make_shots()
 
+stop(server)
+stop(perf_process, wait=True)
 
-def main():
-    server_command = start_server()
-    
-    # Клонирование репозитория FlameGraph, если он еще не был клонирован
-    clone_flamegraph_repo()
-    
-    # Запуск сервера
-    server_process = run(server_command)
-    
-    # Запуск perf record для записи трассировки функций
-    perf_command = "perf record -o perf.data -p {}".format(server_process.pid)
-    perf_process = run(perf_command)
-    
-    # Выполнение обстрелов запросами
-    make_shots()
-    
-    # Остановка perf record
-    stop(perf_process, wait=True)
-    
-    # Остановка сервера
-    stop(server_process)
-    
-    # Построение флеймграфа
-    flamegraph_command = (
-        "perf script -i perf.data | "
-        "./FlameGraph/stackcollapse-perf.pl | "
-        "./FlameGraph/flamegraph.pl > graph.svg"
-    )
-    subprocess.run(flamegraph_command, shell=True)
-    
-    print('Job done')
+convert_process = convert_to_flamegraph()
+stop(convert_process, wait=True)
 
-
-if __name__ == '__main__':
-    main()
+time.sleep(1)
+print('Job done')
